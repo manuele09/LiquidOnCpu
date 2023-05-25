1ff981b1291f34e5132fe50b89c1aa5834de44c3
Emanuele <emacannizzo@gmail.com>
2023-05-25 13:32:38 +0200

    Added Liquid header.

---
+++ liquid.h
@@ -0,0 +1,79 @@
+#ifndef _LIQUID_H
+#define _LIQUID_H 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "neuron.h"
+#include "synapse.h"
+#include "random_utilities.h"
+
+struct Liquid
+{
+    float dt;
+    Neuron exc_neurons;
+    Neuron inh_neurons;
+    Neuron all_neurons;
+
+    Synapse ee_synapses;
+    Synapse ei_synapses;
+    Synapse ii_synapses;
+    Synapse ie_synapses;
+    Synapse all_synapses;
+};
+typedef struct Liquid Liquid;
+
+void connect_liquid(Synapse syn, Neuron layer1, Neuron layer2, int indegree, double J, float dt)
+{
+    double delay = GaussianDistributionClipped(10, 20, 3, 200);
+    double w, tau;
+    if (J >= 0)
+    {
+        w = GaussianDistributionClipped(J, 0.7 * J, 0, 100000);
+        tau = 3;
+    }
+    else
+    {
+        w = GaussianDistributionClipped(J, 0.7 * (-J), -100000, 0);
+        tau = 2;
+    }
+
+    int counter = 0;
+    for (int i = 0; i < layer2.size; i++)
+    {
+        for (int j = 0; j < indegree; j++)
+        {
+            syn.pre_location[counter] = rand() % layer1.size;
+            syn.pre_neuron_idx[counter] = layer1.id[syn.pre_location[counter]];
+            syn.post_neuron_idx[counter] = layer2.id[i];
+            syn.weight[counter] = w;
+            syn.gain[counter] = 1;
+            syn.tau_syn[counter] = tau;
+            syn.delay[counter] = (int)(delay / dt);
+            counter++;
+        }
+    }
+}
+
+Liquid create_liquid(float dt, int n_exc, int n_inh, int n_ee, int n_ei, int n_ii, int n_ie)
+{
+    Liquid liquid;
+    liquid.dt = dt;
+
+    liquid.exc_neurons = create_neurons(n_exc);
+    liquid.inh_neurons = create_neurons(n_inh);
+
+    liquid.ee_synapses = create_synapses(n_exc * n_ee);
+    liquid.ei_synapses = create_synapses(n_inh * n_ei);
+    liquid.ii_synapses = create_synapses(n_inh * n_ii);
+    liquid.ie_synapses = create_synapses(n_exc * n_ie);
+
+    connect_liquid(liquid.ee_synapses, liquid.exc_neurons, liquid.exc_neurons, n_ee, 10 * 5, dt);
+    connect_liquid(liquid.ei_synapses, liquid.exc_neurons, liquid.inh_neurons, n_ei, 10 * 25, dt);
+    connect_liquid(liquid.ii_synapses, liquid.inh_neurons, liquid.inh_neurons, n_ii, 10 * (-20), dt);
+    connect_liquid(liquid.ie_synapses, liquid.inh_neurons, liquid.exc_neurons, n_ie, 10 * (-20), dt);
+
+    return liquid;
+}
+
+#endif
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "liquid.h"
 #include "neuron.h"
 #include "synapse.h"
 #include "visualize.h"
@@ -10,53 +11,26 @@ int main()
 {
     srand(time(NULL));
 
-    printf("Numero: %f\n", GaussianDistributionClipped(0, 1, 0, 10));
+    Liquid liquid = create_liquid(0.1, 100, 80, 2, 2, 1, 1);
+    visualize_synapse(liquid.ei_synapses);
 
-    int n_exc = 80;
-    int n_inh = 20;
+
     
-    //Alloco i Neuroni
-    Neuron layer1 = create_neurons(5);
-    Neuron layer2 = create_neurons(5);
-    Neuron net = create_network(layer1, layer2);
-    //visualize_neuron_layer(net);
+    // //creo il logger
+    // NeuronLogger logger = create_logger(net.size * 10000);
 
-    //Definisco la Connettività
-    int *conn_mat = malloc(sizeof(int) * layer1.size * layer2.size);
-    for (int i = 0; i < layer1.size; i++)
-        for (int j = 0; j < layer2.size; j++)
-            conn_mat[i * layer2.size + j] = 1;
-    
-    //Alloco le Sinapsi
-    Synapse syn = connect(&layer1, &layer2, conn_mat);
-    set_pre_locations(net, syn);
-    //visualize_synapse(syn);
-    
-    //Imposto le correnti di input
-    int *input = malloc(sizeof(int) * net.size);
-    for (int i = 0; i < net.size; i++)
-        input[i] = 56;
-    set_input(net, input);
+    // //simulo
+    // for (int i = 0; i < 10000; i++)
+    // {
+    //     simulate_neurons(&net, i, 0.01, &logger);
+    //     simulate_synapses(&net, &syn, i, 0.01);
+    // }
 
-    //creo il logger
-    NeuronLogger logger = create_logger(net.size * 10000);
+    // //Salvo su file
+    // writeNeuronLogger("prova.txt", logger);
 
-    //simulo
-    for (int i = 0; i < 10000; i++)
-    {
-        update_neurons(&net, i, 0.01, &logger);
-        update_synapses(&net, &syn, i, 0.01);
-    }
-
-    //Salvo su file
-    writeNeuronLogger("prova.txt", logger);
-
-    //Libero la memoria 
-    free_neurons(&layer1);
-    free_neurons(&layer2);
-    free_neurons(&net);
-    free_synapses(&syn);
-    free_neuron_logger(&logger);
+    // //Libero la memoria 
+    // free_neuron_logger(&logger);
 
     return 0;
 }
